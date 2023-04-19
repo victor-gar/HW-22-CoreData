95c08562af770306ee5b499ca07d5e39b784cff2
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-19 16:31:04 +0600

    add UserPresenter

--- CoreDataHW/Sources/Application/AppDelegate.swift
+++ CoreDataHW/Sources/Application/AppDelegate.swift
@@ -10,14 +10,28 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-//    lazy var persistenceStack: PersistenceStack = .init(modelName: "CoreDataModel")
-//    
-//    static let sharedAppDelegate: AppDelegate = {
-//        guard let delegate = UIApplication.shared.delegate as? AppDelegate else {
-//            fatalError("Unexpected app delegate type, did it change? \(String(describing: UIApplication.shared.delegate))")
-//        }
-//        return delegate
-//    }()
+    lazy var persistenceStack: PersistenceStack = .init(modelName: "CoreDataModel")
     
+    static let sharedAppDelegate: AppDelegate = {
+        guard let delegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Unexpected app delegate type, did it change? \(String(describing: UIApplication.shared.delegate))")
+        }
+        return delegate
+    }()
+    
+    var window: UIWindow?
+    let navigationController = UINavigationController()
+    let assemblyBuilder = AssemblyModuleBuilder()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let router = RouterModule(navigationController: navigationController, assemblyBuilder: assemblyBuilder)
+        router.initViewController()
+        window?.overrideUserInterfaceStyle = .light
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+    }
 }
 
